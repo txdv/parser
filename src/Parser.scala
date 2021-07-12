9cbd2afd3fbff3930d97b268977d24583de4e167
Andrius Bentkus <andrius.bentkus@gmail.com>
2021-07-12 21:42:44 +0300

    initial commit

---
+++ src/Parser.scala
@@ -0,0 +1,147 @@
+package bentkus.parser
+
+object Parser {
+  trait Monad[F[_], T] {
+    def map[S](f: T => S): F[S]
+
+    def flatMap[S](f: T => F[S]): F[S]
+  }
+
+  trait Semigroup[A] {
+    def ++(x: => A): A
+  }
+
+  def bind[T, S](p: Parser[T], f: T => Parser[S])(input: String): Result[S] = {
+    for {
+      (value1, input1) <- p(input)
+      result <- f(value1)(input1)
+    } yield result
+  }
+
+  implicit class ParserMonad[T](p: Parser[T]) extends Monad[Parser, T] {
+    def map[S](f: T => S): Parser[S] = {
+      p(_).map {
+        case (result, rest) => (f(result), rest)
+      }
+    }
+
+    def flatMap[S](f: T => Parser[S]): Parser[S] = {
+      bind(p, f)
+    }
+  }
+
+  def plus[T](p: Parser[T], q: Parser[T])(input: String): Result[T] = {
+    p(input) ++ q(input)
+  }
+    
+  implicit class ParserSemigroup[T](p: Parser[T]) extends Semigroup[Parser[T]] {
+    def ++(q: => Parser[T]): Parser[T] = plus(p, q)
+  }
+
+  val item: Parser[Char] = { input: String =>
+    if (input.length == 0) {
+      List.empty
+    } else {
+      List((input(0), input.substring(1)))
+    }
+  }
+
+  def eval[T](result: Result[T]): Option[T] = {
+    result.find { case (value, list) => list.size == 0 }.map(_._1)
+  }
+
+
+  def result[T](v: T): Parser[T] = (input: String) => List((v, input))
+
+  def zero[T]: Parser[T] = (input: String) => List.empty
+
+
+  implicit class ParserWithFilter(p: Parser[Char]) {
+    def withFilter(p: Char => Boolean): Parser[Char] = for {
+      x <- item
+      y <- if (p(x)) result(x) else zero
+    } yield y
+  }
+
+  def sat(p: Char => Boolean): Parser[Char] = {
+    for {
+      x <- item if p(x)
+    } yield x
+  }
+
+  def char(ch: Char): Parser[Char] = sat(_ == ch)
+
+  val digit = sat(ch => ch >= '0' && ch <= '9')
+  val lower = sat(ch => ch >= 'a' && ch <= 'z')
+  val upper = sat(ch => ch >= 'A' && ch <= 'Z')
+
+  val letter = plus(lower, upper)(_)
+
+  val alphanum = plus(letter, digit)(_)
+
+  def ident(str: String): Parser[String] = { input: String =>
+    if (input.startsWith(str)) {
+      List((str, input.substring(str.length)))
+    } else {
+      List.empty
+    }
+  }
+
+  def many[T](p: Parser[T]): Parser[List[T]] = {
+    val res = for {
+      x <- p
+      xs <- many(p)
+    } yield {
+      x :: xs
+    }
+
+    res ++ result(List.empty)
+  }
+
+  def many1[T](p: Parser[T]): Parser[List[T]] = {
+    for {
+      x <- p
+      xs <- many(p)
+    } yield x :: xs
+  }
+
+  def nat: Parser[Int] = {
+    for {
+      xs <- many1(digit)
+    } yield {
+      xs.map(_.toInt - '0'.toInt).foldLeft(0) { case (m, n) => 10 * m + n }
+    }
+  }
+
+  def int: Parser[Int] = {
+    (for {
+      _ <- char('-')
+      n <- nat
+    } yield -n) ++ nat
+  }
+
+
+  def sepBy1[T, S](p: Parser[T], sep: Parser[S]): Parser[List[T]] = for {
+    x <- p
+    xs <- many {
+      for {
+        _ <- sep
+        y <- p
+      } yield y
+    }
+  } yield x :: xs
+
+  val `[` = char('[')
+  val `]` = char(']')
+  val `,` = char(',')
+  val `(` = char('(')
+  val `)` = char(')')
+
+  def bracket[A, B, C](open: Parser[A], p: Parser[B], close: Parser[C]): Parser[B] = for {
+    _ <- open
+    x <- p
+    _ <- close
+  } yield x
+
+  val ints: Parser[List[Int]] = bracket(`[`, sepBy1(int, `,`), `]`)
+}
---
+++ src/ParserSpec.scala
@@ -0,0 +1,107 @@
+package bentkus.parser
+
+import org.scalatest._
+import flatspec._
+import matchers._
+
+import Parser._
+
+class ParserSpec extends AnyFlatSpec with should.Matchers {
+  "item" should "parse singular char string" in {
+    eval(item("1")) should be (Some('1'))
+  }
+
+  "item" should "parse double digit" in {
+    eval(item("12")) should be (None)
+  }
+
+  "value" should "put value into" in {
+    eval(result(1)("")) should be(Some(1))
+  }
+
+  "zero" should "be none" in {
+    eval(zero[Int]("something")) should be(None)
+  }
+
+  "sat" should "parse char in condition" in {
+    eval(sat(_ == 'A')("A")) should be(Some('A'))
+  }
+
+  "sat" should "not parse char in condition" in {
+    eval(sat(_ == 'A')("B")) should be(None)
+  }
+
+  "char" should "parse specified char" in {
+    eval(char('A')("A")) should be(Some('A'))
+  }
+
+  "char" should "not parse unspecified char" in {
+    eval(char('A')("B")) should be(None)
+  }
+
+  "digit" should "parse digits" in {
+    (0 to 9).map(i => ('0'.toInt + i).toChar).foreach { ch =>
+      eval(digit(ch.toString)) should be(Some(ch))
+    }
+  }
+
+  "++" should "combine parsers" in {
+    eval((upper ++ lower)("A")) should be(Some('A'))
+  }
+  
+  "alphanum" should "parse letters and numbers" in {
+    eval(alphanum("A")) should be(Some('A'))
+    eval(alphanum("a")) should be(Some('a'))
+    eval(alphanum("1")) should be(Some('1'))
+  }
+
+  "ident" should "parse specified identifier" in {
+    eval(ident("asd")("asd"))should be(Some("asd"))
+    eval(ident("asd")("asb"))should be(None)
+  }
+
+  "many" should "parse a parser multiple times" in {
+    eval(many(digit)("123")) should be(Some(List('1', '2', '3')))
+  }
+
+  "many" should "succeed with zero items" in {
+    eval(many(digit)("")) should be(Some(List()))
+  }
+
+  "many1" should "succed only with at least 1 item" in {
+    eval(many1(digit)("")) should be(None)
+    eval(many1(digit)("1")) should be(Some(List('1')))
+  }
+
+  "nat" should "parse natural numbers" in {
+    eval(nat("1")) should be(Some(1))
+    eval(nat("123")) should be(Some(123))
+    eval(nat("asd")) should be(None)
+  }
+
+  "int" should "parse negative numbers as well" in {
+    eval(int("123")) should be(Some(123))
+    eval(int("-123")) should be(Some(-123))
+  }
+
+  "sepBy1" should "parse strings with seperators" in {
+    eval(sepBy1(int, `,`)("1,2,3")) should be(Some(List(1, 2, 3)))
+  }
+
+  "special chars" should "parse special chars" in {
+    eval(`[`("[")) should be(Some('['))
+    eval(`]`("]")) should be(Some(']'))
+    eval(`,`(",")) should be(Some(','))
+    eval(`)`(")")) should be(Some(')'))
+    eval(`(`("(")) should be(Some('('))
+  }
+
+  "bracket" should "enclose parser with brackets" in {
+    eval(bracket(`(`, int, `)`)("(123)")) should be(Some(123))
+  }
+  
+  "ints" should "parse ints enclosed with [] and seperated by commas" in {
+    eval(ints("[1,2,3,42]")) should be(Some(List(1, 2, 3, 42)))
+
+  }
+}
